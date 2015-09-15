de7d27d3a4a6275b05ec925a3c04b8ea86ba4bcc
juanbmillet <juanbmillet@users.noreply.github.com>
2015-09-15 16:19:57 -0300

    Update EMRTest.php

--- tests/EMRTest.php
+++ tests/EMRTest.php
@@ -16,30 +16,30 @@ class TarjetaTest extends PHPUnit_Framework_TestCase{
 	public function setUp(){
 		$this->tarjeta1 = new tarjeta(0,1);						// $0, tarjeta comun
 		$this->tarjeta2 = new tarjeta(150,0);						// $150, tarjeta medio boleto
-		$tarjeta4 = new tarjeta(0,0);						// $0, tarjeta medio boleto
-		$tarjeta3 = new tarjeta(100,1);						// $100, tarjeta comun
-		$colectivo144 = new colectivo("rosariobus",144,54321);			//colectivo 144
-		$colectivo153 = new colectivo("semtur",153,12345);			//colectivo 154
-		$horario = date("d.m.y H:i:s");						// hora actual
-		$horariotransbordo = date("d.m.y H:i:s", strtotime('+1 hour'));		//hora actual + 1 hora
+		$this->tarjeta4 = new tarjeta(0,0);						// $0, tarjeta medio boleto
+		$this->tarjeta3 = new tarjeta(100,1);						// $100, tarjeta comun
+		$this->colectivo144 = new colectivo("rosariobus",144,54321);			//colectivo 144
+		$this->colectivo153 = new colectivo("semtur",153,12345);			//colectivo 154
+		$this->horario = date("d.m.y H:i:s");						// hora actual
+		$this->horariotransbordo = date("d.m.y H:i:s", strtotime('+1 hour'));		//hora actual + 1 hora
 	}
 
 	public function testPagarBoleto(){
 
 	$this->assertFalse($this->tarjeta1->pagarboleto($horario, $horario, $colectivo144));		// saldo insuficiente comun
 
-	$this->assertEquals($this->$tarjeta3->pagarboleto($horario, $horario , $colectivo144), true);		// pasaje comun
-	$this->assertEquals($this->$tarjeta3->pagarboleto($horario, $horariotransbordo, $colectivo153), true);	// transbordo comun
-	$this->assertEquals($this->$tarjeta3->saldo(), 92.35);							// 100-5.75-1.90=92.35
+	$this->assertEquals($this->tarjeta3->pagarboleto($horario, $horario , $colectivo144), true);		// pasaje comun
+	$this->assertEquals($this->tarjeta3->pagarboleto($horario, $horariotransbordo, $colectivo153), true);	// transbordo comun
+	$this->assertEquals($this->tarjeta3->saldo(), 92.35);							// 100-5.75-1.90=92.35
 
 
-	$this->assertEquals($this->$tarjeta4->pagarboleto($horario, $horario, $colectivo153), false);		// saldo insuficiente medio boleto
+	$this->assertEquals($this->tarjeta4->pagarboleto($horario, $horario, $colectivo153), false);		// saldo insuficiente medio boleto
 
-	$this->assertEquals($this->$tarjeta2->pagarboleto(strtotime("03:00:00"), strtotime("01:00:00"), $colectivo144), true); 	//pasaje normal con tarjeta medio boleto
-	$this->assertEquals($this->$tarjeta2->pagarboleto(strtotime("03:10:00"), strtotime("04:00:00"), $colectivo153), true);	//transbordo normal con tarjeta medio boleto
-	$this->assertEquals($this->$tarjeta2->pagarboleto($horario, strtotime("04:10:00"), $colectivo144), true);			// pasaje medio boleto
-	$this->assertEquals($this->$tarjeta2->pagarboleto($horario, $horariotransbordo, $colectivo153), true);			// transbordo medioboleto
-	$this->assertEquals($this->$tarjeta2->saldo(), 138.49);									// 150 - 5,75 - 1,90 - 2,90 - 0,96 = 138.49
+	$this->assertEquals($this->tarjeta2->pagarboleto(strtotime("03:00:00"), strtotime("01:00:00"), $colectivo144), true); 	//pasaje normal con tarjeta medio boleto
+	$this->assertEquals($this->tarjeta2->pagarboleto(strtotime("03:10:00"), strtotime("04:00:00"), $colectivo153), true);	//transbordo normal con tarjeta medio boleto
+	$this->assertEquals($this->tarjeta2->pagarboleto($horario, strtotime("04:10:00"), $colectivo144), true);			// pasaje medio boleto
+	$this->assertEquals($this->tarjeta2->pagarboleto($horario, $horariotransbordo, $colectivo153), true);			// transbordo medioboleto
+	$this->assertEquals($this->tarjeta2->saldo(), 138.49);									// 150 - 5,75 - 1,90 - 2,90 - 0,96 = 138.49
 	
 	
 
