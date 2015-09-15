89bab7b32b0eb91f38b078d6950e560444878548
juanbmillet <juanbmillet@users.noreply.github.com>
2015-09-15 16:18:23 -0300

    Update EMRTest.php

--- tests/EMRTest.php
+++ tests/EMRTest.php
@@ -15,7 +15,7 @@ class TarjetaTest extends PHPUnit_Framework_TestCase{
 
 	public function setUp(){
 		$this->tarjeta1 = new tarjeta(0,1);						// $0, tarjeta comun
-		$tarjeta2 = new tarjeta(150,0);						// $150, tarjeta medio boleto
+		$this->tarjeta2 = new tarjeta(150,0);						// $150, tarjeta medio boleto
 		$tarjeta4 = new tarjeta(0,0);						// $0, tarjeta medio boleto
 		$tarjeta3 = new tarjeta(100,1);						// $100, tarjeta comun
 		$colectivo144 = new colectivo("rosariobus",144,54321);			//colectivo 144
