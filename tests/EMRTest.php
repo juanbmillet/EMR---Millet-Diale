1a9d71fe9bc59a93fa9c3bd87f3c81ee188fee71
juanbmillet <juanbmillet@users.noreply.github.com>
2015-09-15 15:20:23 -0300

    Update EMRTest.php

--- tests/EMRTest.php
+++ tests/EMRTest.php
@@ -18,20 +18,20 @@ class TarjetaTest extends PHPUnit_Framework_TestCase{
 
 	public function testPagarBoleto(){
 
-	$this->assert($tarjeta1->pagarboleto($horario, $horario, $colectivo144)==false);		// saldo insuficiente comun
+	$this->assertEquals($tarjeta1->pagarboleto($horario, $horario, $colectivo144), false);		// saldo insuficiente comun
 
-	$this->assert($tarjeta3->pagarboleto($horario, $horario , $colectivo144)==true);		// pasaje comun
-	$this->assert($tarjeta3->pagarboleto($horario, $horariotransbordo, $colectivo153)==true);	// transbordo comun
-	$this->assert($tarjeta3->saldo()==92.35);							// 100-5.75-1.90=92.35
+	$this->assertEquals($tarjeta3->pagarboleto($horario, $horario , $colectivo144), true);		// pasaje comun
+	$this->assertEquals($tarjeta3->pagarboleto($horario, $horariotransbordo, $colectivo153), true);	// transbordo comun
+	$this->assertEquals($tarjeta3->saldo(), 92.35);							// 100-5.75-1.90=92.35
 
 
-	$this->assert($tarjeta4->pagarboleto($horario, $horario, $colectivo153)==false);		// saldo insuficiente medio boleto
+	$this->assertEquals($tarjeta4->pagarboleto($horario, $horario, $colectivo153), false);		// saldo insuficiente medio boleto
 
-	$this->assert($tarjeta2->pagarboleto(strtotime("03:00:00"), strtotime("01:00:00"), $colectivo144)==true); 	//pasaje normal con tarjeta medio boleto
-	$this->assert($tarjeta2->pagarboleto(strtotime("03:10:00"), strtotime("04:00:00"), $colectivo153)==true);	//transbordo normal con tarjeta medio boleto
-	$this->assert($tarjeta2->pagarboleto($horario, strtotime("04:10:00"), $colectivo144)==true);			// pasaje medio boleto
-	$this->assert($tarjeta2->pagarboleto($horario, $horariotransbordo, $colectivo153)==true);			// transbordo medioboleto
-	$this->assert($tarjeta2->saldo()==138.49);									// 150 - 5,75 - 1,90 - 2,90 - 0,96 = 138.49
+	$this->assertEquals($tarjeta2->pagarboleto(strtotime("03:00:00"), strtotime("01:00:00"), $colectivo144), true); 	//pasaje normal con tarjeta medio boleto
+	$this->assertEquals($tarjeta2->pagarboleto(strtotime("03:10:00"), strtotime("04:00:00"), $colectivo153), true);	//transbordo normal con tarjeta medio boleto
+	$this->assertEquals($tarjeta2->pagarboleto($horario, strtotime("04:10:00"), $colectivo144), true);			// pasaje medio boleto
+	$this->assertEquals($tarjeta2->pagarboleto($horario, $horariotransbordo, $colectivo153), true);			// transbordo medioboleto
+	$this->assertEquals($tarjeta2->saldo(), 138.49);									// 150 - 5,75 - 1,90 - 2,90 - 0,96 = 138.49
 	
 	
 
@@ -40,19 +40,19 @@ class TarjetaTest extends PHPUnit_Framework_TestCase{
 	public function testRecarga(){
 
 		$tarjeta1->recarga(-12);
-		$this->assert($tarjeta1->saldo()==0);				// Si recarga negativo no cambia el valor de saldo
+		$this->assertEquals($tarjeta1->saldo(), 0);				// Si recarga negativo no cambia el valor de saldo
 
 		$tarjeta1->recarga(100);
-		$this->assert($tarjeta1->saldo()==100);			// 0+100=100
+		$this->assertEquals($tarjeta1->saldo(), 100);			// 0+100=100
 
 		$tarjeta2->recarga(200);
-		$this->assert($tarjeta2->saldo()==384);			// 150+200+34=384
+		$this->assertEquals($tarjeta2->saldo(), 384);			// 150+200+34=384
 
 		$tarjeta3->recarga(400);
-		$this->assert($tarjeta3->saldo()==592);			// 100+400+92=592
+		$this->assertEquals($tarjeta3->saldo(), 592);			// 100+400+92=592
 
 		$tarjeta3->recarga(0);
-		$this->assert($tarjeta3->saldo()==593);			// 592+0=592
+		$this->assertEquals($tarjeta3->saldo(), 593);			// 592+0=592
 
 	}
 }
